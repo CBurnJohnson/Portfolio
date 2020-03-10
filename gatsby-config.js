d876c4ccd90cfbd650dadd93c6640cfd8be3df0c
Cole Johnson <cburnjohnson@gmail.com>
2020-03-09 19:00:37 -0700

    initial setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
     siteMetadata: {
-        title: `Gatsby Default Starter`,
-        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-        author: `@gatsbyjs`
+        title: `Cole Johnson`,
+        description: `Cole Johnson's Portfolio`,
+        author: `Cole Johnson`
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
