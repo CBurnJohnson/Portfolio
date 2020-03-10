94e863f2495dcfbde8b6a491f47adbe705ac8a69
Cole Johnson <cburnjohnson@gmail.com>
2020-03-09 20:15:09 -0700

    particle tweaking

--- src/components/banner/ParticlesContainer.js
+++ src/components/banner/ParticlesContainer.js
@@ -1,121 +1,134 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import Particles from 'react-particles-js';
 
 const ParticlesContainer = () => {
+    useEffect(() => {
+        var canvas = document.querySelector('#particles'),
+            ctx = canvas.getContext('2d'),
+            particles = [],
+            amount = 0,
+            mouse = { x: 0, y: 0 },
+            radius = 1;
+
+        var colors = ['#00AAFF', '#00C6FF', '#00DCE4', '#0BECB8', '#A0F68B'];
+
+        var ww = (canvas.width = window.innerWidth);
+        var wh = (canvas.height = window.innerHeight);
+
+        function Particle(x, y) {
+            this.x = Math.random() * ww;
+            this.y = Math.random() * wh;
+            this.dest = {
+                x: x,
+                y: y
+            };
+            this.r = Math.random() * 5 + 2;
+            this.vx = (Math.random() - 0.5) * 20;
+            this.vy = (Math.random() - 0.5) * 20;
+            this.accX = 0;
+            this.accY = 0;
+            this.friction = Math.random() * 0.05 + 0.94;
+
+            this.color = colors[Math.floor(Math.random() * 6)];
+        }
+
+        Particle.prototype.render = function() {
+            this.accX = (this.dest.x - this.x) / 1000;
+            this.accY = (this.dest.y - this.y) / 1000;
+            this.vx += this.accX;
+            this.vy += this.accY;
+            this.vx *= this.friction;
+            this.vy *= this.friction;
+
+            this.x += this.vx;
+            this.y += this.vy;
+
+            ctx.fillStyle = this.color;
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.r, Math.PI * 2, false);
+            ctx.fill();
+
+            var a = this.x - mouse.x;
+            var b = this.y - mouse.y;
+
+            var distance = Math.sqrt(a * a + b * b);
+            if (distance < radius * 70) {
+                this.accX = (this.x - mouse.x) / 100;
+                this.accY = (this.y - mouse.y) / 100;
+                this.vx += this.accX;
+                this.vy += this.accY;
+            }
+        };
+
+        function onMouseMove(e) {
+            mouse.x = e.clientX;
+            mouse.y = e.clientY;
+        }
+
+        function onTouchMove(e) {
+            if (e.touches.length > 0) {
+                mouse.x = e.touches[0].clientX;
+                mouse.y = e.touches[0].clientY;
+            }
+        }
+
+        function onTouchEnd(e) {
+            mouse.x = -9999;
+            mouse.y = -9999;
+        }
+
+        function initScene() {
+            ww = canvas.width = window.innerWidth;
+            wh = canvas.height = window.innerHeight;
+
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            ctx.font = 'bold ' + ww / 10 + 'px sans-serif';
+            ctx.textAlign = 'center';
+            ctx.fillText('Developer', ww / 2, wh / 2);
+
+            var data = ctx.getImageData(0, 0, ww, wh).data;
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.globalCompositeOperation = 'screen';
+
+            particles = [];
+            for (var i = 0; i < ww; i += Math.round(ww / 150)) {
+                for (var j = 0; j < wh; j += Math.round(ww / 150)) {
+                    if (data[(i + j * ww) * 4 + 3] > 150) {
+                        particles.push(new Particle(i, j));
+                    }
+                }
+            }
+            amount = particles.length;
+        }
+
+        function onMouseClick() {
+            radius++;
+            if (radius === 5) {
+                radius = 0;
+            }
+        }
+
+        function render(a) {
+            requestAnimationFrame(render);
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            for (var i = 0; i < amount; i++) {
+                particles[i].render();
+            }
+        }
+
+        window.addEventListener('resize', initScene);
+        window.addEventListener('mousemove', onMouseMove);
+        window.addEventListener('touchmove', onTouchMove);
+        window.addEventListener('click', onMouseClick);
+        window.addEventListener('touchend', onTouchEnd);
+        initScene();
+        requestAnimationFrame(render);
+    });
+
     return (
         <div className='particles-container'>
-            <Particles
-                params={{
-                    particles: {
-                        number: {
-                            value: 80,
-                            density: {
-                                enable: true,
-                                value_area: 800
-                            }
-                        },
-                        color: {
-                            value: '#ffffff'
-                        },
-                        shape: {
-                            type: 'circle',
-                            stroke: {
-                                width: 0,
-                                color: '#000000'
-                            },
-                            polygon: {
-                                nb_sides: 5
-                            },
-                            image: {
-                                src: 'img/github.svg',
-                                width: 100,
-                                height: 100
-                            }
-                        },
-                        opacity: {
-                            value: 0.5,
-                            random: false,
-                            anim: {
-                                enable: false,
-                                speed: 1,
-                                opacity_min: 0.1,
-                                sync: false
-                            }
-                        },
-                        size: {
-                            value: 3,
-                            random: true,
-                            anim: {
-                                enable: false,
-                                speed: 40,
-                                size_min: 0.1,
-                                sync: false
-                            }
-                        },
-                        line_linked: {
-                            enable: true,
-                            distance: 150,
-                            color: '#ffffff',
-                            opacity: 0.4,
-                            width: 1
-                        },
-                        move: {
-                            enable: true,
-                            speed: 6,
-                            direction: 'none',
-                            random: false,
-                            straight: false,
-                            out_mode: 'out',
-                            bounce: false,
-                            attract: {
-                                enable: false,
-                                rotateX: 600,
-                                rotateY: 1200
-                            }
-                        }
-                    },
-                    interactivity: {
-                        detect_on: 'canvas',
-                        events: {
-                            onhover: {
-                                enable: true,
-                                mode: 'repulse'
-                            },
-                            onclick: {
-                                enable: true,
-                                mode: 'push'
-                            },
-                            resize: true
-                        },
-                        modes: {
-                            grab: {
-                                distance: 400,
-                                line_linked: {
-                                    opacity: 1
-                                }
-                            },
-                            bubble: {
-                                distance: 400,
-                                size: 40,
-                                duration: 2,
-                                opacity: 8,
-                                speed: 3
-                            },
-                            repulse: {
-                                distance: 200,
-                                duration: 0.4
-                            },
-                            push: {
-                                particles_nb: 4
-                            },
-                            remove: {
-                                particles_nb: 2
-                            }
-                        }
-                    },
-                    retina_detect: true
-                }}
-            />
+            <canvas id='particles'></canvas>
         </div>
     );
 };
