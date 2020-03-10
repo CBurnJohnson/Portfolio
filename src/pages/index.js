06f7dc8c4ce0fc1a3fb850fb3e18c7981772d275
Cole Johnson <cburnjohnson@gmail.com>
2020-03-09 19:30:33 -0700

    initial particle setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../styles/styles.scss';
 
 // Component Imports
 import Image from '../components/image';
 import ParticlesContainer from '../components/banner/ParticlesContainer';
 
 const IndexPage = () => (
-    <div>
+    <div className='home-page'>
         <ParticlesContainer />
     </div>
 );
