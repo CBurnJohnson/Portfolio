40f6768ca48593c7cba322c45713e563221fb0cb
Cole Johnson <cburnjohnson@gmail.com>
2020-03-11 11:29:43 -0700

    update

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const Projects = ({ projects }) => {
+    const onClick = () => {
+        console.log('functon');
+    };
+
     return (
         <ul className='projects-list'>
             {projects.map(project => (
-                <li>
+                <li onClick={onClick}>
                     <h3>{project.name}</h3>
                     <p>{project.description}</p>
                 </li>
