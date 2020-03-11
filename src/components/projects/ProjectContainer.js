f5e6ecf683adf6e346ce3d08f688a66f3867f032
Cole Johnson <cburnjohnson@gmail.com>
2020-03-10 17:16:13 -0700

    projects change based on what you click

--- src/components/projects/ProjectContainer.js
+++ src/components/projects/ProjectContainer.js
@@ -98,6 +98,38 @@ const ProjectContainer = () => {
         }
     ]);
 
+    const [cNetProjects, setCNetProejcts] = useState([
+        {
+            name: 'C SHARP BRo',
+            description:
+                ' Nam nisl est, aliquam sit amet erat faucibus, vestibulum euismod leo. Donec quis nulla eget augue mollis lacinia. Maecenas consectetur dictum ex eu fermentum. Donec sit amet erat dignissim, dignissim ipsum eleifend, pretium augue. Maecenas euismod massa sed lorem pharetra facilisis. Nam ac nulla a enim tincidunt egestas vitae et nulla. Curabitur eu felis luctus, aliquam orci quis, suscipit turpis. Nullam sed lacinia felis, sed ultrices augue. Cras elementum posuere tellus egestas vehicula.',
+            github: 'https://github.com/cburnjohnson/watch-n-track',
+            deploy: 'https://watchntrack.herokuapp.com/',
+            image: 'https://source.unsplash.com/random'
+        },
+        {
+            name: 'Phone Book',
+            description: 'describes the project',
+            github: 'https://github.com/cburnjohnson/watch-n-track',
+            deploy: 'https://watchntrack.herokuapp.com/',
+            image: 'https://source.unsplash.com/random'
+        },
+        {
+            name: 'Project 3',
+            description: 'describes the project',
+            github: 'https://github.com/cburnjohnson/watch-n-track',
+            deploy: 'https://watchntrack.herokuapp.com/',
+            image: 'https://source.unsplash.com/random'
+        },
+        {
+            name: 'Project 4',
+            description: 'describes the project',
+            github: 'https://github.com/cburnjohnson/watch-n-track',
+            deploy: 'https://watchntrack.herokuapp.com/',
+            image: 'https://source.unsplash.com/random'
+        }
+    ]);
+
     const [phpProjects, setPhpProjects] = useState([
         {
             name: 'PHP',
@@ -142,18 +174,53 @@ const ProjectContainer = () => {
         }
     });
 
+    const onMernClick = () => {
+        setCurrentProjects({
+            ...currentProjects,
+            active: 'mern'
+        });
+    };
+
+    const onReactClick = () => {
+        setCurrentProjects({
+            ...currentProjects,
+            active: 'react'
+        });
+    };
+
+    const onJavascriptClick = () => {
+        setCurrentProjects({
+            ...currentProjects,
+            active: 'javascript'
+        });
+    };
+
+    const onPhpClick = () => {
+        setCurrentProjects({
+            ...currentProjects,
+            active: 'php'
+        });
+    };
+
+    const onCNetClick = () => {
+        setCurrentProjects({
+            ...currentProjects,
+            active: 'c#net'
+        });
+    };
+
     return (
         <div className='projects-container'>
             <div className='projects-buttons'>
-                <button>MERN Stack</button>
+                <button onClick={onMernClick}>MERN Stack</button>
 
-                <button>React</button>
+                <button onClick={onReactClick}>React</button>
 
-                <button>Vanilla JS</button>
+                <button onClick={onJavascriptClick}>Vanilla JS</button>
 
-                <button>PHP</button>
+                <button onClick={onPhpClick}>PHP</button>
 
-                <button>C#/.NET</button>
+                <button onClick={onCNetClick}>C#/.NET</button>
             </div>
             <h1 className='projects-header'>Projects</h1>
             <Projects
@@ -166,6 +233,8 @@ const ProjectContainer = () => {
                         ? javascriptProjects
                         : currentProjects.active === 'php'
                         ? phpProjects
+                        : currentProjects.active === 'c#net'
+                        ? cNetProjects
                         : currentProjects.noProjects
                 }
             />
