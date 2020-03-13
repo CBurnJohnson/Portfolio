9b6ead0a912039217a6831f660704193fc34be6a
Cole Johnson <cburnjohnson@gmail.com>
2020-03-13 15:18:10 -0700

    initial image

--- src/components/projects/ActiveProject.js
+++ src/components/projects/ActiveProject.js
@@ -8,39 +8,57 @@ const ActiveProject = () => {
 
     const initialProject = mernProjects[0];
 
+    const data = useStaticQuery(graphql`
+        query {
+            placeholderImage: file(
+                relativePath: { eq: "gatsby-astronaut.png" }
+            ) {
+                childImageSharp {
+                    fluid(maxWidth: 300) {
+                        ...GatsbyImageSharpFluid
+                    }
+                }
+            }
+        }
+    `);
+
     return (
-        <div className="active-project-container">
+        <div className='active-project-container'>
             {activeProject !== null ? (
-                <div className="active-project">
+                <div className='active-project'>
                     <h2>{activeProject.name}</h2>
-                    <img src={activeProject.image} alt="Active project image" />
+                    <div className='main-image'>
+                        <Img
+                            fluid={data.placeholderImage.childImageSharp.fluid}
+                        />
+                    </div>
                     <p>Technology Used: {activeProject.techUsed}</p>
                     <p>{activeProject.description}</p>
 
-                    <a className="button" href={activeProject.deploy}>
+                    <a className='button' href={activeProject.deploy}>
                         View
                     </a>
-                    <a className="button" href={activeProject.github}>
+                    <a className='button' href={activeProject.github}>
                         Github
                     </a>
                 </div>
             ) : (
-                <div className="active-project">
+                <div className='active-project'>
                     <h2>{initialProject.name}</h2>
-                    <div className="active-project-image-container">
+                    <div className='main-image'>
                         <img
                             src={initialProject.image}
-                            alt="Active project image"
+                            alt='Active project image'
                         />
                     </div>
 
                     <p>Technology Used: {initialProject.techUsed}</p>
                     <p>{initialProject.description}</p>
 
-                    <a className="button" href={initialProject.deploy}>
+                    <a className='button' href={initialProject.deploy}>
                         View
                     </a>
-                    <a className="button" href={initialProject.github}>
+                    <a className='button' href={initialProject.github}>
                         Github
                     </a>
                 </div>
