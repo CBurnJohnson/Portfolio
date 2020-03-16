9382d13377cbd84fda81a56e46375b3574b8f07d
Cole Johnson <cburnjohnson@gmail.com>
2020-03-15 23:44:49 -0700

    about

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,25 +3,28 @@ import { Link } from 'gatsby';
 
 const Navbar = () => {
     return (
-        <nav className='navbar'>
+        <nav className="navbar">
             <h1>
-                <Link to='/' activeClassName='navbar-active'>
+                <Link to="/" activeClassName="navbar-active">
                     Cole Johnson
                 </Link>
             </h1>
             <ul>
                 <li>
-                    <Link activeClassName='navbar-active' to='/projects'>
+                    <Link to="/contact">About</Link>
+                </li>
+                <li>
+                    <Link activeClassName="navbar-active" to="/projects">
                         Projects
                     </Link>
                 </li>
                 <li>
-                    <Link activeClassName='navbar-active' to='/resume'>
+                    <Link activeClassName="navbar-active" to="/resume">
                         Resume
                     </Link>
                 </li>
                 <li>
-                    <Link to='/contact' activeClassName='navbar-active'>
+                    <Link to="/contact" activeClassName="navbar-active">
                         Contact
                     </Link>
                 </li>
