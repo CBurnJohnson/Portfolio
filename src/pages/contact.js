ce73c30367709a25aa920865a76458648a689724
Cole Johnson <cburnjohnson@gmail.com>
2020-03-16 21:40:39 -0700

    style sheet import

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import '../styles/styles.scss';
 import Navbar from '../components/layout/Navbar';
 import ContactForm from '../components/contact/ContactForm';
 import About from '../components/contact/About';
 
 const contact = () => {
     return (
-        <div className='container'>
+        <div className="container">
             <Navbar />
-            <div className='contact-flex'>
-                <div className='about-wrapper'>
-                    <div className='about-container'>
+            <div className="contact-flex">
+                <div className="about-wrapper">
+                    <div className="about-container">
                         <h2>About Me</h2>
                         <About />
                     </div>
                 </div>
-                <div className='contact-wrapper'>
-                    <div className='contact-container'>
+                <div className="contact-wrapper">
+                    <div className="contact-container">
                         <h2>Contact Me</h2>
                         <ContactForm />
                     </div>
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import '../styles/styles.scss';
 import Navbar from '../components/layout/Navbar';
 import Resume from "../static/Cole's-Resume.pdf";
 
