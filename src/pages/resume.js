264425fa3897d6ed431fd9925588c00f6c69a234
Cole Johnson <cburnjohnson@gmail.com>
2020-03-24 17:32:25 -0700

    added particles to resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -2,17 +2,24 @@ import React from 'react';
 import '../styles/styles.scss';
 import Navbar from '../components/layout/Navbar';
 import Resume from "../static/Cole's-Resume.pdf";
+import BackgroundParticles from '../components/contact/BackgroundParticles';
 
 const resume = () => {
     return (
-        <div className="container">
-            <Navbar />
-
-            <div className="resume-container">
-                <h2>My Resume</h2>
-                <embed src={Resume} />
+        <>
+            <div id='background-particles'>
+                <BackgroundParticles />
             </div>
-        </div>
+
+            <div className='container resume'>
+                <Navbar />
+
+                <div className='resume-container'>
+                    <h2>My Resume</h2>
+                    <embed src={Resume} />
+                </div>
+            </div>
+        </>
     );
 };
 
